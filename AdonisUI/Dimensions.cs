85902b0e3efc40686a96c5459afdefca68404597
Benjamin Rühl <dev.ruehl@gmail.com>
2018-05-01 00:56:18 +0200

    Add global dimensions including corner radius and border thickness

---
+++ AdonisUI/Dimensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdonisUI
+{
+    public static class Dimensions
+    {
+        public static ComponentResourceKey CornerRadius => new ComponentResourceKey(typeof(Dimensions), "CornerRadius");
+
+        public static ComponentResourceKey BorderThickness => new ComponentResourceKey(typeof(Dimensions), "BorderThickness");
+    }
+}
