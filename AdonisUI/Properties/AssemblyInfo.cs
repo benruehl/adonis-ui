9a8b96cd388f412880390c3e4112e0161d301db5
Benjamin Rühl <dev.ruehl@gmail.com>
2018-06-02 16:36:05 +0200

    Setup nuget deployment

--- AdonisUI/Properties/AssemblyInfo.cs
+++ AdonisUI/Properties/AssemblyInfo.cs
@@ -4,21 +4,21 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("AdonisUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core theme definitions for AdonisUI - a lightweight UI toolkit for WPF applications offering classic but enhanced windows visuals")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("benruehl")]
 [assembly: AssemblyProduct("AdonisUI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Benjamin Rühl")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 //Um mit dem Erstellen lokalisierbarer Anwendungen zu beginnen, legen Sie
@@ -41,15 +41,15 @@ using System.Windows;
 )]
 
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+// Version information for an assembly consists of the following four values:
 //
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
+//      Major Version
+//      Minor Version
+//      Build Number
 //      Revision
 //
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// übernehmen, indem Sie "*" eingeben:
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Scripts/NuGetPack.ps1
@@ -0,0 +1,3 @@
+nuget pack ..\AdonisUI\AdonisUi.csproj -Build -Properties Configuration=Release
+nuget pack ..\AdonisUI.ClassicTheme\AdonisUI.ClassicTheme.csproj -Build -Properties Configuration=Release
+Read-Host -Prompt "Press Enter to exit"
