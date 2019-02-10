fac51ec8735c9cbf973cfd4e36951715a34e068a
Benjamin Rühl <dev.ruehl@gmail.com>
2019-02-10 01:04:23 +0100

    Add extension that allows disabling progress bar animation

---
+++ AdonisUI/Extensions/ProgressBarExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace AdonisUI.Extensions
+{
+    public class ProgressBarExtension
+    {
+        public static bool GetIsProgressAnimationEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsProgressAnimationEnabledProperty);
+        }
+
+        public static void SetIsProgressAnimationEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsProgressAnimationEnabledProperty, value);
+        }
+
+        public static readonly DependencyProperty IsProgressAnimationEnabledProperty = DependencyProperty.RegisterAttached("IsProgressAnimationEnabled", typeof(bool), typeof(ProgressBarExtension), new PropertyMetadata(false));
+    }
+}
