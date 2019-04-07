c97a1457c633e95a1083425c896d3ee3188c099a
Benjamin Rühl <dev.ruehl@gmail.com>
2019-04-07 13:25:05 +0200

    Add ability to set content to progress bars

---
+++ AdonisUI/Converters/RectConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AdonisUI.Converters
+{
+    public class RectConverter
+        : IMultiValueConverter
+    {
+        public static RectConverter Instance = new RectConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 0)
+                return new Rect();
+
+            if (values.Length == 1 && values[0] is Size)
+                return new Rect((Size)values[0]);
+
+            if (values.Length == 2 && values[0] is double && values[1] is double)
+                return new Rect(new Size((double) values[0], (double) values[1]));
+
+            if (values.Length == 2 && values[0] is Point && values[1] is Point)
+                return new Rect((Point)values[0], (Point)values[1]);
+
+            if (values.Length == 2 && values[0] is Point && values[1] is Size)
+                return new Rect((Point)values[0], (Size)values[1]);
+
+            if (values.Length == 2 && values[0] is Point && values[1] is Vector)
+                return new Rect((Point)values[0], (Vector)values[1]);
+
+            if (values.Length == 4 && values[0] is double && values[1] is double && values[2] is double && values[3] is double)
+                return new Rect((double)values[0], (double)values[1], (double)values[2], (double)values[3]);
+
+            return Binding.DoNothing;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AdonisUI/Extensions/ProgressBarExtension.cs
+++ AdonisUI/Extensions/ProgressBarExtension.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Media;
 
 namespace AdonisUI.Extensions
 {
     public class ProgressBarExtension
     {
+        public static object GetContent(DependencyObject obj)
+        {
+            return (object)obj.GetValue(ContentProperty);
+        }
+
+        public static void SetContent(DependencyObject obj, object value)
+        {
+            obj.SetValue(ContentProperty, value);
+        }
+
+        public static DataTemplate GetContentTemplate(DependencyObject obj)
+        {
+            return (DataTemplate)obj.GetValue(ContentTemplateProperty);
+        }
+
+        public static void SetContentTemplate(DependencyObject obj, DataTemplate value)
+        {
+            obj.SetValue(ContentTemplateProperty, value);
+        }
+
+        public static Brush GetForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(ForegroundProperty);
+        }
+
+        public static void SetForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(ForegroundProperty, value);
+        }
+
+        public static Brush GetProgressAnimationForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(ProgressAnimationForegroundProperty);
+        }
+
+        public static void SetProgressAnimationForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(ProgressAnimationForegroundProperty, value);
+        }
+
         public static bool GetIsProgressAnimationEnabled(DependencyObject obj)
         {
             return (bool)obj.GetValue(IsProgressAnimationEnabledProperty);
@@ -17,6 +58,14 @@ namespace AdonisUI.Extensions
         {
             obj.SetValue(IsProgressAnimationEnabledProperty, value);
         }
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.RegisterAttached("Content", typeof(object), typeof(ProgressBarExtension), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.RegisterAttached("ContentTemplate", typeof(DataTemplate), typeof(ProgressBarExtension), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ProgressAnimationForegroundProperty = DependencyProperty.RegisterAttached("ProgressAnimationForeground", typeof(Brush), typeof(ProgressBarExtension), new PropertyMetadata(System.Windows.Media.Brushes.Black));
+
+        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.RegisterAttached("Foreground", typeof(Brush), typeof(ProgressBarExtension), new PropertyMetadata(System.Windows.Media.Brushes.Black));
 
         public static readonly DependencyProperty IsProgressAnimationEnabledProperty = DependencyProperty.RegisterAttached("IsProgressAnimationEnabled", typeof(bool), typeof(ProgressBarExtension), new PropertyMetadata(false));
     }
