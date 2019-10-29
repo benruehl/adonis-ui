1b8fe9be8ecb25372b4114570a562438eb10076b
Bert Proesmans <bproesmans@hotmail.com>
2019-10-29 19:35:37 +0100

    New converter to apply unary minus on each component of Thickness

---
+++ AdonisUI/Converters/ThicknessToInverseThicknessConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AdonisUI.Converters
+{
+    public class ThicknessToInverseThicknessConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new ThicknessToInverseThicknessConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Thickness valueAsThickness = (Thickness)value;
+
+            return new Thickness(
+                -valueAsThickness.Left,
+                -valueAsThickness.Top,
+                -valueAsThickness.Right,
+                -valueAsThickness.Bottom);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
