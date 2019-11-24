2e887cb9b14f01e6b0c222748640ccdf8f4bb5ac
Benjamin Rühl <dev.ruehl@gmail.com>
2019-11-24 22:00:16 +0100

    Add scenario for issue to IssueDialog

--- AdonisUI.Demo/ViewModels/IssueDialogViewModel.cs
+++ AdonisUI.Demo/ViewModels/IssueDialogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using AdonisUI.Demo.Framework;
@@ -9,6 +10,35 @@ namespace AdonisUI.Demo.ViewModels
     class IssueDialogViewModel
         : ViewModel
     {
-        
+        private readonly ObservableCollection<ItemViewModel> _items = new ObservableCollection<ItemViewModel>();
+        public ReadOnlyObservableCollection<ItemViewModel> Items { get; set; }
+
+        public IssueDialogViewModel()
+        {
+            Items = new ReadOnlyObservableCollection<ItemViewModel>(_items);
+
+            AddDummyItems(3);
+        }
+
+        private void AddDummyItems(int count)
+        {
+            foreach (ItemViewModel item in CreateDummyItems(count))
+            {
+                _items.Add(item);
+            }
+        }
+
+        private IEnumerable<ItemViewModel> CreateDummyItems(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var item = new ItemViewModel
+                {
+                    Name = $"Item {i + 1}",
+                };
+
+                yield return item;
+            }
+        }
     }
 }
