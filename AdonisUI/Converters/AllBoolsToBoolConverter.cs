5371ef6f9b9ad5e669885a8a4e3c8a9a13c58ecc
Benjamin Rühl <dev.ruehl@gmail.com>
2019-12-20 00:48:53 +0100

    Fix data grid row error icon leading to differing row header sizes when virtualization is active

---
+++ AdonisUI/Converters/AllBoolsToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AdonisUI.Converters
+{
+    /// <summary>
+    /// Takes multiple bools as input and returns true if all bools are true.
+    /// </summary>
+    public class AllBoolsToBoolConverter
+        : IMultiValueConverter
+    {
+        public static AllBoolsToBoolConverter Instance = new AllBoolsToBoolConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.OfType<bool>().All(x => x);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdonisUI/Extensions/DataGridExtension.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace AdonisUI.Extensions
+{
+    /// <summary>
+    /// Provides attached behaviors related to the DataGrid control.
+    /// </summary>
+    public class DataGridExtension
+    {
+        /// <summary>
+        /// Gets the value of the <see cref="HasAnyRowErrorProperty"/> attached property of the specified DataGrid.
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(DataGrid))]
+        public static bool GetHasAnyRowError(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(HasAnyRowErrorProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="HasAnyRowErrorProperty"/> attached property of the specified DataGrid.
+        /// </summary>
+        private static void SetHasAnyRowError(DependencyObject obj, bool value)
+        {
+            obj.SetValue(HasAnyRowErrorPropertyKey, value);
+        }
+
+        /// <summary>
+        /// Gets the value of the <see cref="HadAnyRowErrorProperty"/> attached property of the specified DataGrid.
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(DataGrid))]
+        public static bool GetHadAnyRowError(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(HadAnyRowErrorProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="HadAnyRowErrorProperty"/> attached property of the specified DataGrid.
+        /// </summary>
+        private static void SetHadAnyRowError(DependencyObject obj, bool value)
+        {
+            obj.SetValue(HadAnyRowErrorPropertyKey, value);
+        }
+
+        /// <summary>
+        /// Gets the value of the <see cref="IsReportingErrorsToDataGridProperty"/> attached property of the specified DataGridRow.
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(DataGridRow))]
+        public static bool GetIsReportingErrorsToDataGrid(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsReportingErrorsToDataGridProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="IsReportingErrorsToDataGridProperty"/> attached property of the specified DataGridRow.
+        /// </summary>
+        public static void SetIsReportingErrorsToDataGrid(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsReportingErrorsToDataGridProperty, value);
+        }
+
+        private static readonly DependencyPropertyKey HasAnyRowErrorPropertyKey = DependencyProperty.RegisterAttachedReadOnly("HasAnyRowError", typeof(bool), typeof(DataGridExtension), new PropertyMetadata(false));
+
+        /// <summary>
+        /// A DependencyProperty that will be true if any DataGridRow in the DataGrid has <see cref="Validation.HasErrorProperty"/> set to true.
+        /// </summary>
+        public static readonly DependencyProperty HasAnyRowErrorProperty = HasAnyRowErrorPropertyKey.DependencyProperty;
+        
+        private static readonly DependencyPropertyKey HadAnyRowErrorPropertyKey = DependencyProperty.RegisterAttachedReadOnly("HadAnyRowError", typeof(bool), typeof(DataGridExtension), new PropertyMetadata(false));
+
+        /// <summary>
+        /// A DependencyProperty that will be true if any DataGridRow in the DataGrid has or had at least once <see cref="Validation.HasErrorProperty"/> set to true.
+        /// </summary>
+        public static readonly DependencyProperty HadAnyRowErrorProperty = HadAnyRowErrorPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// A DependencyProperty that needs to be true if the DataGridRow should update the <see cref="HasAnyRowErrorProperty"/> and <see cref="HadAnyRowErrorProperty"/> properties of its DataGrid.
+        /// </summary>
+        public static readonly DependencyProperty IsReportingErrorsToDataGridProperty = DependencyProperty.RegisterAttached("IsReportingErrorsToDataGrid", typeof(bool), typeof(DataGridExtension), new PropertyMetadata(false, OnIsReportingErrorsToDataGridPropertyChanged));
+
+        private static void OnIsReportingErrorsToDataGridPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DataGridRow dataGridRow = (DataGridRow) d;
+            DataGrid parentDataGrid = FindVisualParent<DataGrid>(dataGridRow);
+            DependencyPropertyDescriptor hasErrorProperty = DependencyPropertyDescriptor.FromProperty(Validation.HasErrorProperty, typeof(DataGridRow));
+
+            UpdateDataGridHasAnyRowError(parentDataGrid);
+
+            if ((bool) e.NewValue)
+            {
+                hasErrorProperty.AddValueChanged(dataGridRow, OnDataGridRowHasErrorChanged);
+            }
+            else
+            {
+                hasErrorProperty.RemoveValueChanged(dataGridRow, OnDataGridRowHasErrorChanged);
+            }
+        }
+
+        private static void OnDataGridRowHasErrorChanged(object sender, EventArgs e)
+        {
+            DataGridRow dataGridRow = (DataGridRow) sender;
+            DataGrid parentDataGrid = FindVisualParent<DataGrid>(dataGridRow);
+
+            UpdateDataGridHasAnyRowError(parentDataGrid);
+            UpdateDataGridHadAnyRowError(parentDataGrid);
+        }
+
+        private static void UpdateDataGridHasAnyRowError(DataGrid dataGrid)
+        {
+            SetHasAnyRowError(dataGrid, GetDataGridRows(dataGrid).Where(GetIsReportingErrorsToDataGrid).Any(Validation.GetHasError));
+        }
+
+        private static void UpdateDataGridHadAnyRowError(DataGrid dataGrid)
+        {
+            if (!GetHadAnyRowError(dataGrid))
+                SetHadAnyRowError(dataGrid, GetDataGridRows(dataGrid).Where(GetIsReportingErrorsToDataGrid).Any(Validation.GetHasError));
+        }
+
+        private static IEnumerable<DataGridRow> GetDataGridRows(DataGrid dataGrid)
+        {
+            foreach (object item in dataGrid.ItemsSource)
+            {
+                if (dataGrid.ItemContainerGenerator.ContainerFromItem(item) is DataGridRow row)
+                    yield return row;
+            }
+        }
+
+        private static T FindVisualParent<T>(DependencyObject current) where T : DependencyObject
+        {
+            current = VisualTreeHelper.GetParent(current);
+
+            while (current != null)
+            {
+                if (current is T)
+                    return (T)current;
+                
+                current = VisualTreeHelper.GetParent(current);
+            };
+
+            return null;
+        }
+    }
+}
