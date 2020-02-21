66136303496010548ab66a6e898b6fd7e257fb84
M0n7y5 <krystof.montilla@protonmail.com>
2020-02-21 15:48:59 +0100

    Fix for Net Core self contained WPF apps

--- AdonisUI/Controls/AdonisWindow.cs
+++ AdonisUI/Controls/AdonisWindow.cs
@@ -157,7 +157,7 @@ namespace AdonisUI.Controls
 
         private BitmapSource GetApplicationIcon()
         {
-            Icon appIcon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly()?.ManifestModule.FullyQualifiedName);
+            Icon appIcon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly()?.ManifestModule.FullyQualifiedName);
 
             if (appIcon == null)
                 return null;
